7dd64d256cc4f3db92a912f263d3a63fc0ec2dfd
Resam Taghipour <resam.t@gmail.com>
2023-12-04 17:05:00 +0330

    BottomNavigationBar

--- lib/app/components/main_components/app_bottom_navigation_bar.dart
+++ lib/app/components/main_components/app_bottom_navigation_bar.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../../core/app_routing/routing.dart';
+import '../../../core/app_extensions/data_types_extensions/extension_icon.dart';
+import '../../../core/app_routing/app_routes.dart';
+import '../../../core/core_functions.dart';
+import '../../../data/data_models/core_data_models/app_page_detail/app_page_detail.dart';
+import '../../../data/info/app_page_details.dart';
 import '../../../data/resources/app_colors.dart';
-import 'app_bottom_navigation_bar_data.dart';
+import '../../../data/resources/app_icons.dart';
 
 class AppBottomNavigationBar extends StatefulWidget {
   final int? selectedIndex;
@@ -14,19 +18,16 @@ class AppBottomNavigationBar extends StatefulWidget {
 }
 
 class _AppBottomNavigationBarState extends State<AppBottomNavigationBar> {
-
   RxInt selectedIndex = 0.obs;
+
+  List<AppPageDetail> pages = <AppPageDetail>[
+    AppPageDetails.homepage,
+    AppPageDetails.settings,
+  ];
 
   void _onItemTap(int index) {
     selectedIndex.value = index;
-    switch(index) {
-      case 0 : goToHomePage();
-      case 1 : goToHomePage();
-      case 2 : goToHomePage();
-      case 3 : goToHomePage();
-      case 4 : goToSettingsPage();
-      default: break;
-    }
+    goToPage(pages[index]);
     Get.reload();
   }
 
@@ -41,6 +42,35 @@ class _AppBottomNavigationBarState extends State<AppBottomNavigationBar> {
         onTap: (index) => _onItemTap(index),
         selectedItemColor: AppColors.bottomBarSelected,
         unselectedItemColor: AppColors.bottomBarUnselected,
-        items: AppBottomNavigationBarData().getData());
+        items: List<BottomNavigationBarItem>.generate(pages.length,
+            (index) => _generateBottomNavigationBarItem(pages[index])));
   }
+
+  BottomNavigationBarItem _generateBottomNavigationBarItem(
+          AppPageDetail pageDetail) =>
+      BottomNavigationBarItem(
+          activeIcon: CircleAvatar(
+              backgroundColor: AppColors.textNormalDark,
+              foregroundColor: AppColors.textNormalDark,
+              child: _createIcon(pageDetail.pageRoute!)
+                  .withColor(AppColors.textNormalLight)),
+          icon: _createIcon(pageDetail.pageRoute!),
+          label: _createLabel(pageDetail.pageName));
+
+  Icon _createIcon(String pageName) {
+    Icon icon = const Icon(Icons.not_interested);
+    switch (pageName) {
+      case (AppRoutes.homePage):
+        icon = AppIcons.bottomNavigationHomepage;
+        break;
+      case (AppRoutes.settings):
+        icon = AppIcons.bottomNavigationSettings;
+        break;
+      default:
+        break;
+    }
+    return icon;
+  }
+
+  String? _createLabel(String? label) => label;
 }
