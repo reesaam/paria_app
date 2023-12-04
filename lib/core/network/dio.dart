4804404a19bacfaa2366a5579b75d46380432205
Resam Taghipour <resam.t@gmail.com>
2023-12-04 15:14:03 +0330

    Network and Dio

---
+++ lib/core/network/dio.dart
@@ -0,0 +1,27 @@
+import 'package:injectable/injectable.dart';
+import 'package:dio/dio.dart';
+
+import '../../data/info/app_urls.dart';
+import '../../data/resources/app_texts.dart';
+
+@module
+abstract class DioCore {
+
+  int get _defaultTimeOut => 15;
+
+  @singleton
+  Dio get dio => _getDio();
+
+  _getDio() {
+    var dio = Dio(BaseOptions(baseUrl: AppURLs.apiBaseUrl));
+    dio.options.headers.addAll( {
+      AppTexts.dioHeaderContentType: AppTexts.dioHeaderContentTypeData,
+      AppTexts.dioHeaderConnection: AppTexts.dioHeaderConnectionData,
+    });
+    dio.options.connectTimeout = Duration(seconds: _defaultTimeOut);
+    dio.options.receiveTimeout = Duration(seconds: _defaultTimeOut);
+    dio.options.sendTimeout = Duration(seconds: _defaultTimeOut);
+    return dio;
+  }
+
+}
