750af0e6ea91c164e06a30c4eebf6d0d828762be
Resam Taghipour <resam.t@gmail.com>
2023-12-04 17:04:41 +0330

    Dependency Injection Commit

--- lib/core/app_bindings/storage_bindings.dart
+++ lib/core/app_bindings/storage_bindings.dart
@@ -1,4 +1,3 @@
-import 'package:base_flutter_clean_getx_app/core/dependency_injection/di.dart';
 import 'package:get/get.dart';
 
 import '../../data/storage/app_local_storage.dart';
@@ -7,7 +6,7 @@ import '../../data/storage/app_shared_preferences.dart';
 class AppStorageBindings extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<AppSharedPreferences>(() => diCore<AppSharedPreferences>(), fenix: true);
-    Get.lazyPut<AppLocalStorage>(() => diCore<AppLocalStorage>(), fenix: true);
+    Get.lazyPut<AppSharedPreferences>(() => AppSharedPreferences(), fenix: true);
+    Get.lazyPut<AppLocalStorage>(() => AppLocalStorage(), fenix: true);
   }
 }
--- lib/core/app_extensions/data_models_extensions/extension_settings.dart
+++ lib/core/app_extensions/data_models_extensions/extension_settings.dart
@@ -1,19 +1,17 @@
-import 'package:base_flutter_clean_getx_app/core/dependency_injection/di.dart';
 import 'package:get/get.dart';
 
 import '../../../data/data_models/core_data_models/app_settings_data/app_setting_data.dart';
 import '../../../data/storage/app_local_storage.dart';
 
-var _storage = diCore<AppLocalStorage>();
 
 extension RxStorage on Rx<AppSettingData> {
-  void get saveOnStorage async => await _storage.saveSettings(value);
-  Rx<AppSettingData> get loadFromStorage => _storage.loadSettings().obs;
+  void get saveOnStorage async => await AppLocalStorage.to.saveSettings(value);
+  Rx<AppSettingData> get loadFromStorage => AppLocalStorage.to.loadSettings().obs;
 }
 
 extension Storage on AppSettingData {
-  void get saveOnStorage async => await _storage.saveSettings(this);
-  AppSettingData get loadFromStorage => _storage.loadSettings();
+  void get saveOnStorage async => await AppLocalStorage.to.saveSettings(this);
+  AppSettingData get loadFromStorage => AppLocalStorage.to.loadSettings();
 }
 
 extension RxClear on Rx<AppSettingData> {
