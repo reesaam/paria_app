b6d568e58f5ddba9fc97c4fc7cc6f9445ab49832
Resam Taghipour <resam.t@gmail.com>
2024-01-02 16:09:23 +0330

    features rename Refactor

---
+++ lib/features/settings/presentation/widgets/settings_widgets.dart
@@ -0,0 +1,48 @@
+import 'package:base_flutter_clean_getx_app/core/app_extensions/data_types_extensions/extension_locale.dart';
+import 'package:base_flutter_clean_getx_app/core/app_localization.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/core_widgets.dart';
+import '../../../../data/resources/app_paddings.dart';
+import '../../../../data/resources/app_text_styles.dart';
+import '../../../../app/components/general_widgets/app_dividers.dart';
+
+class SettingsWidgets {
+  Widget widgetSettingSection(Widget title, List<Widget> widgets) => Padding(
+      padding: AppPaddings.settingsSection,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        title,
+        AppDividers.settingsDivider,
+        Column(children: widgets),
+      ]));
+
+  Widget widgetSectionTitle(String text) =>
+      Text(text, style: AppTextStyles.settingsSectionTitle);
+
+  Widget widgetSettingItem(
+          String text, Widget? leading, Function? wholeItemFunction) =>
+      InkWell(
+          onTap: wholeItemFunction == null ? null : () => wholeItemFunction(),
+          child: Padding(
+              padding: AppPaddings.settingsItem,
+              child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(text, style: AppTextStyles.settingsSectionItem),
+                    leading ?? shrinkSizedBox,
+                  ])));
+
+  Widget widgetSelectLanguageForm(Function function) => Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: List.generate(
+          AppLocalization.languages.length,
+          (index) => InkWell(
+              child: LayoutBuilder(
+                  builder: (context, constraints) => Container(
+                      width: constraints.maxWidth,
+                      padding: AppPaddings.modalItems,
+                      child: Text(
+                          AppLocalization.languages[index].getLanguageName))),
+              onTap: () => function(index))));
+}
