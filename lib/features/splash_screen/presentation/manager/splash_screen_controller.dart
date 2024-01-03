c902b8c327dcdf859ee653cb4853e92a744d963a
Resam Taghipour <resam.t@gmail.com>
2024-01-03 12:05:22 +0330

    SplashScreen Internet Status Implementation

--- lib/features/splash_screen/presentation/manager/splash_screen_controller.dart
+++ lib/features/splash_screen/presentation/manager/splash_screen_controller.dart
@@ -1,10 +1,19 @@
+import '../../../../app/functional_components/connectivity/connectivity.dart';
 import '../../../../core/elements/core_controller.dart';
 import '../../../../data/info/app_page_details.dart';
 
 class SplashScreenController extends CoreController {
 
+  late bool internetStatus;
+
   @override
   void pageInit() {
     pageDetail = AppPageDetails.splashScreen;
+  }
+
+  @override
+  void onReadyFunction() async {
+    internetStatus = await ConnectionChecker().checkInternet();
+    super.onReadyFunction();
   }
 }
