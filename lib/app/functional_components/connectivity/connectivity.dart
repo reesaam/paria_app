60eae215cf54940310ae6d42153a9af0ada0b837
Resam Taghipour <resam.t@gmail.com>
2024-01-03 10:45:00 +0330

    InternetCheck Package and Implementation

---
+++ lib/app/functional_components/connectivity/connectivity.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:get/get.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+
+class ConnectionChecker {
+  static ConnectionChecker get to => Get.find();
+
+  Future<bool> checkInternet() async =>
+      await InternetConnection().hasInternetAccess;
+
+  Future<InternetStatus> checkInternetStatus() async =>
+      await InternetConnection().internetStatus;
+
+  internetListener({Function? function}) => InternetConnection()
+      .onStatusChange
+      .listen((status) {})
+      .onData((data) => function == null ? data : function());
+
+  Future<ConnectivityResult> checkConnection() async =>
+      await Connectivity().checkConnectivity();
+
+  connectionListener({Function? function}) => Connectivity()
+      .onConnectivityChanged
+      .listen((status) {})
+      .onData((data) => function == null ? data : function());
+}
