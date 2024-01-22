0cfdce973ce81c74f6edce9e601192ef593987e4
Resam Taghipour <resam.t@gmail.com>
2024-01-22 16:44:14 +0330

    ConnectionChecker

--- lib/app/functional_components/connectivity/connectivity.dart
+++ lib/app/functional_components/connectivity/connectivity.dart
@@ -1,14 +1,16 @@
 import 'dart:async';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_base_clean_getx_app/data/info/app_defaults.dart';
 import 'package:get/get.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 class ConnectionChecker {
   static ConnectionChecker get to => Get.find();
 
-  Future<bool> checkInternet() async =>
-      await InternetConnection().hasInternetAccess;
+  Future<bool> checkInternet() async => await InternetConnection()
+      .hasInternetAccess
+      .timeout(defaultConnectionTimeOut);
 
   Future<InternetStatus> checkInternetStatus() async =>
       await InternetConnection().internetStatus;
