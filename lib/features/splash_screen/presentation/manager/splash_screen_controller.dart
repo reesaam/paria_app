689493242c2117059f0819eba9e4301f69db9950
Resam Taghipour <resam.t@gmail.com>
2024-02-10 15:33:13 +0330

    SplashScreen Controller

--- lib/features/splash_screen/presentation/manager/splash_screen_controller.dart
+++ lib/features/splash_screen/presentation/manager/splash_screen_controller.dart
@@ -1,16 +1,40 @@
+import 'package:get/get.dart';
+
 import '../../../../app/components/main_components/app_dialogs.dart';
 import '../../../../app/functional_components/connectivity/connectivity.dart';
+import '../../../../app/functional_components/persmissions/permissions.dart';
 import '../../../../core/app_localization.dart';
+import '../../../../core/app_routing/app_routes.dart';
 import '../../../../core/core_functions.dart';
 import '../../../../core/elements/core_controller.dart';
+import '../../../../data/info/app_info.dart';
 import '../../../../data/info/app_page_details.dart';
+import '../../../../data/resources/app_logos.dart';
 
 class SplashScreenController extends CoreController {
   late bool internetStatus;
+  late String availableUpdate;
+  late String permissionsStatus;
+
+  late String logoSource;
+  late String appName;
+  late String appVersion;
+
+  @override
+  void dataInit() async {
+    // clearAppData();
+    // loadAppData();
+    availableUpdate = await checkAvailableVersion();
+    permissionsStatus = await AppPermissions.to.checkAllPermissions();
+  }
 
   @override
   void pageInit() {
     pageDetail = AppPageDetails.splashScreen;
+
+    logoSource = AppLogos.appAnimatedLogo;
+    appName = AppInfo.appName;
+    appVersion = '${Texts.to.version}: ${AppInfo.appCurrentVersion}';
   }
 
   @override
@@ -25,4 +49,20 @@ class SplashScreenController extends CoreController {
             false);
     super.onReadyFunction();
   }
+
+  void goToNextPage() async {
+    await Future.delayed(const Duration(seconds: 4));
+    availableUpdate == AppInfo.appCurrentVersion
+        ? showUpdateDialog()
+        : goToHomepage();
+  }
+
+  showUpdateDialog() {
+    AppDialogs().appAlertDialogWithOkCancel(Texts.to.updateNewVersion,
+        Texts.to.updateApprove, goToUpdatePage, false);
+  }
+
+  goToHomepage() => Get.offAndToNamed(AppRoutes.homePage);
+  goToUpdatePage() => Get.offAndToNamed(AppRoutes.homePage)
+      ?.then((value) => Get.toNamed(AppRoutes.update));
 }
