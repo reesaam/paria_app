d1e158152a1dce20e19f3f40ef4d1b6f81bc6dca
Resam Taghipour <resam.t@gmail.com>
2024-02-10 14:52:29 +0330

    Project Initialization

--- lib/data/info/app_info.dart
+++ lib/data/info/app_info.dart
@@ -1,15 +1,15 @@
 class AppInfo {
-  static String get appName => 'Base Flutter Clean GetX App';
-  static String get appNameInitials => 'BFCGA';
+  static String get appName => 'Paria Assistant App';
+  static String get appNameInitials => 'PAA';
   static String get website => '';
   static List<String> get versions => [
-        '0.0.1', //Initialization
+        '0.1.0', //Initialization
       ];
   static String get appCurrentVersion => versions.last;
   static int get appVersionsCounter => versions.length;
 
   static String get baseUrl => 'resam-t.ir';
-  static String get subDomain => 'basefluttercleangetxapp';
+  static String get subDomain => 'pariaapp';
   static String get fileName => 'download file name';
 
   static bool get checkUpdate => false;
