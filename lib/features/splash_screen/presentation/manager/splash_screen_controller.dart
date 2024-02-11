f29ac6349ffffa34c0e0b4bf2d96f99f36f24acc
Resam Taghipour <resam.t@gmail.com>
2024-02-11 16:29:44 +0330

    SplashScreen

--- lib/features/splash_screen/presentation/manager/splash_screen_controller.dart
+++ lib/features/splash_screen/presentation/manager/splash_screen_controller.dart
@@ -1,13 +1,34 @@
 import 'package:flutter_base_clean_getx_app/app/components/main_components/app_dialogs.dart';
 import 'package:flutter_base_clean_getx_app/core/app_localization.dart';
+import 'package:get/get.dart';
 
 import '../../../../app/functional_components/connectivity/connectivity.dart';
+import '../../../../app/functional_components/permissions/permissions.dart';
+import '../../../../core/app_routing/app_routes.dart';
 import '../../../../core/core_functions.dart';
 import '../../../../core/elements/core_controller.dart';
+import '../../../../data/info/app_info.dart';
 import '../../../../data/info/app_page_details.dart';
 
 class SplashScreenController extends CoreController {
   late bool internetStatus;
+  late String availableUpdate;
+  late String permissionsStatus;
+
+  late String logoSource;
+  late String appName;
+  late String appVersion;
+
+  @override
+  void dataInit() async {
+    // clearAppData();
+    // loadAppData();
+    availableUpdate = await checkAvailableVersion();
+    permissionsStatus = await AppPermissions.to.checkAllPermissions();
+    appDebugPrint(availableUpdate);
+    appDebugPrint(permissionsStatus);
+    appDebugPrint('SplashScreen DataInit Finish');
+  }
 
   @override
   void pageInit() {
@@ -26,4 +47,19 @@ class SplashScreenController extends CoreController {
             false);
     super.onReadyFunction();
   }
+
+  void goToNextPage() async {
+    await Future.delayed(const Duration(seconds: 4));
+    availableUpdate == AppInfo.appCurrentVersion
+        ? goToHomepage()
+        : showUpdateDialog();
+  }
+
+  showUpdateDialog() => AppDialogs().appAlertDialogWithOkCancel(
+      Texts.to.updateNewVersion, Texts.to.updateApprove, goToUpdatePage, false);
+
+  goToHomepage() => Get.offAndToNamed(AppRoutes.homePage);
+
+  goToUpdatePage() => Get.offAndToNamed(AppRoutes.homePage)
+      ?.then((value) => Get.toNamed(AppRoutes.update));
 }
