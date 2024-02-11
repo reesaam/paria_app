dec7939261e02c93004b337afb63bc695121043a
Resam Taghipour <resam.t@gmail.com>
2024-02-11 15:45:51 +0330

    Bindings

--- lib/core/app_bindings/core_bindings.dart
+++ lib/core/app_bindings/core_bindings.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 import '../../core/app_bindings/localization_bindings.dart';
 import 'admin_bindings.dart';
 import 'app_bindings.dart';
+import 'components_bindings.dart';
+import 'repositories_bindings.dart';
 import 'storage_bindings.dart';
 
 class CoreBindings implements Bindings {
@@ -10,7 +12,9 @@ class CoreBindings implements Bindings {
   void dependencies() {
     AppStorageBindings().dependencies(); //Storage
     AppLocalizationBindings().dependencies(); //AppLocalization
+    AppRepositoriesBindings().dependencies(); //Repositories
+    AppComponentsBindings().dependencies(); //Components
     AppBindings().dependencies(); // Pages
-    AdminBindings().dependencies(); // Admin related
+    AdminBindings().dependencies(); // Admin related// Admin related
   }
 }
